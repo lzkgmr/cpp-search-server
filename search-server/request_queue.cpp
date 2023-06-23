c2d59dac0c7e9b4749db9d3f3f435ffb19f797c9
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2023-06-23 11:36:13 +0300

    s6(1)

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,19 +1,8 @@
 #include "request_queue.h"
 
-
 RequestQueue::RequestQueue(const SearchServer& search_server) 
     : server_(search_server)         
 {
-}
-
-template <typename DocumentPredicate>
-vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
-    vector<Document> result = server_.FindTopDocuments(raw_query, document_predicate);
-    requests_.push_back({result.empty(), result});
-    if (requests_.size() > min_in_day_) {
-        requests_.pop_front();
-    }
-    return result;
 }
     
 vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
