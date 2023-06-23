7d57a668dca7327f23f16852c625edbb9db0c3c5
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2023-06-23 15:05:44 +0300

    s6(3)

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -2,20 +2,18 @@
 
 void RemoveDuplicates(SearchServer& search_server) {
     set<int> ids_to_delete;
-    map<int, set<string>> docs_words;
+    set<set<string>> docs_words;
     for (const int doc_id : search_server) {
         const auto& words_freqs = search_server.GetWordFrequencies(doc_id); 
         set<string> words; 
         for (const auto& [word, freq] : words_freqs) {
             words.insert(word);
         }
-        if (count_if(docs_words.begin(), docs_words.end(), [words](const auto& x) {
-            return x.second == words;
-        })) {
+        if (docs_words.count(words) > 0) {
             cout << "Found duplicate document id "s << doc_id << endl;
             ids_to_delete.insert(doc_id);
         } else {
-            docs_words[doc_id] = words;
+            docs_words.insert(words);
         }
         
     }
