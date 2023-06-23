6a74419eb04a39e7b27c4ad409b1218a65137193
lzkgmr <91319276+lzkgmr@users.noreply.github.com>
2023-06-23 11:34:44 +0300

    s6(1)

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -24,3 +24,13 @@ private:
     const static int min_in_day_ = 1440;
     const SearchServer& server_;
 };
+
+template <typename DocumentPredicate>
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+    vector<Document> result = server_.FindTopDocuments(raw_query, document_predicate);
+    requests_.push_back({result.empty(), result});
+    if (requests_.size() > min_in_day_) {
+        requests_.pop_front();
+    }
+    return result;
+}
